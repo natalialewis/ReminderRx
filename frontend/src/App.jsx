f66340e3bd4e54089fe89741ac9cf7d16b158930
Natalia Lewis <a02296603@usu.edu>
2025-06-22 17:05:22 -0600

    Added pill background component to App so it is across all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import PillBackground from "./components/PillBackground";
 
 function App() {
 
   return (
     <BrowserRouter>
+      <PillBackground />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
